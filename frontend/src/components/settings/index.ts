e54778722e99407171bf49ef5451480840a84b6f
染川 瞳 <me@hareru.org>
2026-07-27 08:26:54 +0900

    fix(ci): sort FieldSelect export for eslint perfectionist

--- frontend/src/components/settings/index.ts
+++ frontend/src/components/settings/index.ts
@@ -8,12 +8,12 @@ export { InfoCard } from './InfoCard'
 export { ButtonItem } from './items/ButtonItem'
 export { CheckboxGroupItem } from './items/CheckboxGroupItem'
 export { CheckboxItem } from './items/CheckboxItem'
+export { FieldSelect } from './items/FieldSelect'
 export { InputItem } from './items/InputItem'
 
 export { NumberGroupItem } from './items/NumberGroupItem'
 export { NumberItem } from './items/NumberItem'
 export { ProviderItem } from './items/ProviderItem'
-export { FieldSelect } from './items/FieldSelect'
 export { SelectItem } from './items/SelectItem'
 // 具体设置项组件
 export { SwitchItem } from './items/SwitchItem'
